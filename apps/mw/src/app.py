fa09084be7aa23010d4598c3b3289bff9e611b2a
Offonika <arseniikeshtov@gmail.com>
2025-10-24 08:35:27 +0300

    feat(mw): register chatkit v1 router

--- apps/mw/src/app.py
+++ apps/mw/src/app.py
@@ -19,12 +19,12 @@ from apps.mw.src.api.dependencies import (
 from apps.mw.src.config import get_settings
 from apps.mw.src.api.routes import b24_calls as b24_calls_router
 from apps.mw.src.api.routes import call_registry as call_registry_router
-from apps.mw.src.api.routes import chatkit as chatkit_router
+from apps.mw.src.api.routes import chatkit as chatkit_routes_router
 from apps.mw.src.api.routes import returns as returns_router
 from apps.mw.src.api.routes import stt_admin as stt_admin_router
 from apps.mw.src.api.routes import system as system_router
 from apps.mw.src.api.routes import ww as ww_router
-from apps.mw.src.api.routers import chatkit as chatkit_v1_router
+from apps.mw.src.api.routers import chatkit as chatkit_router
 from apps.mw.src.api.schemas import Error, Health
 from apps.mw.src.health import get_health_payload
 from apps.mw.src.observability import (
@@ -57,8 +57,8 @@ app.include_router(call_registry_router.router)
 app.include_router(returns_router.router)
 app.include_router(stt_admin_router.router)
 app.include_router(ww_router.router)
+app.include_router(chatkit_routes_router.router)
 app.include_router(chatkit_router.router)
-app.include_router(chatkit_v1_router.router)
 
 
 @app.on_event("startup")
